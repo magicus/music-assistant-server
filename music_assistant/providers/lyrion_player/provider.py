80e48a46681754049d66fd2da7033703bad9f6d5
Magnus Ihse Bursie <mag@icus.se>
2026-09-11 14:59:43 +0200

    Don't show host/port in config

--- music_assistant/providers/lyrion_player/provider.py
+++ music_assistant/providers/lyrion_player/provider.py
@@ -54,23 +54,7 @@ class LyrionPlayerProvider(PlayerProvider):
 
     async def get_config_entries(self) -> tuple[ConfigEntry, ...]:
         """Return Config entries to configure this provider."""
-        configured_host = self.get_configured_host() or ""
-        configured_port = self.get_configured_port(default=None)
         return (
-            ConfigEntry(
-                key=CONF_LMS_HOST,
-                type=ConfigEntryType.STRING,
-                required=False,
-                read_only=True,
-                value=configured_host,
-            ),
-            ConfigEntry(
-                key=CONF_LMS_PORT,
-                type=ConfigEntryType.INTEGER,
-                required=False,
-                read_only=True,
-                value=configured_port,
-            ),
             ConfigEntry(
                 key=CONF_FALLBACK_POLLING,
                 type=ConfigEntryType.BOOLEAN,
